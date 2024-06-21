6b66db37710e8bae356fdb65d80e92e9a5f43e02
Ángel Saúl Correa Martínez <angelsaul@MacBook-Air-de-Angel.local>
2024-06-21 11:09:38 -0600

    actualizacion 1.1

--- Estacionamiento_SensoresIR/Estacionamiento_SensoresIR.ino
+++ Estacionamiento_SensoresIR/Estacionamiento_SensoresIR.ino
@@ -1,34 +1,35 @@
-// int infrarrojo = 8;
-// int valor = 1;
-
-// void setup() {
-//   Serial.begin(9600);
-//   pinMode(infrarrojo, INPUT);
-// }
-
-// void loop() {
-//   valor = random(0, 2);  // Genera un número aleatorio entre 0 y 1
-//   if (valor == 1) {
-//     Serial.println("Obstaculo Detectado");  // Usar comillas dobles para cadenas de texto
-//   } else {
-//     Serial.println("No hay obstaculo");  // Añadido para ver el otro caso
-//   }
-//   delay(500);
-// }
-int infrarrojo = 0;
-int valor = 1;
+const int numSensores = 20; // se va a ajustar en base a lo que nos digan los de implemetnacion y documentacion
+const int sensores[numSensores] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+int estadoSensores[numSensores];
 
 void setup() {
   Serial.begin(9600);
-  pinMode(infrarrojo, INPUT);
+
+  //ciclo for para establecer los pines de los sensores como input
+  for (i = 0; i < numSensores; i++){ 
+    pinMode(sensores[i], INPUT)
+  }
+
 }
 
 void loop() {
-  valor =  digitalRead(infrarrojo); 
-  if (valor == LOW) {
-    Serial.println("Obstaculo Detectado");  
-  } else {
-    Serial.println("No hay obstaculo"); 
+  int lugaresLibres = 0; //valor inicial
+  int lugaresOcupados = 0; // valor inicial
+
+  // Ciclo for parta leer los valores de cada sensor 
+  for (int i = 0; i < numSensores; i++){
+    estadoSensores[i] = digitalRead(sensores[i]);
+    //Si estan ocupados, se incrementa uno en la variable lugares ocupados , si no , se aumenta uno en los libres
+    if (estadoSensores[i] == LOW){ //checar que si sea LOW cuando esta ocupado
+      lugaresOcupados++;
+    }else{
+      lugaresLibres++;
+    }
   }
-  delay(500);
+  Serial.print("Lugares Libres: ");
+  Serial.println(lugaresLibres);
+  Serial.print("Lugares Ocupados: ");
+  Serial.println(lugaresOcupados);
+
+  delay(10000)//verificar si el delay es correcto con pruebas 
 }
