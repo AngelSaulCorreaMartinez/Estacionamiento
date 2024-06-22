7a214f20400d2b3e1c97766dafd24853c3bdd46c
Ángel Saúl Correa Martínez <angelsaul@MacBook-Air-de-Angel.local>
2024-06-21 18:40:35 -0600

    Conteo de lugares listo para Infrarrojos

--- Estacionamiento_SensoresIR/Estacionamiento_SensoresIR.ino
+++ Estacionamiento_SensoresIR/Estacionamiento_SensoresIR.ino
@@ -1,35 +1,49 @@
 const int numSensores = 20; // se va a ajustar en base a lo que nos digan los de implemetnacion y documentacion
 const int sensores[numSensores] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 int estadoSensores[numSensores];
+int estadoAnteriorSensores[numSensores];
+int lugaresLibres = numSensores; //valor inicial equivalente al numero de cajones (se piensa que todos esta libres inicialmente)
+int lugaresOcupados = 0; // valor inicial
 
 void setup() {
   Serial.begin(9600);
 
   //ciclo for para establecer los pines de los sensores como input
-  for (i = 0; i < numSensores; i++){ 
-    pinMode(sensores[i], INPUT)
+  for (int i = 0; i < numSensores; i++){ 
+    pinMode(sensores[i], INPUT);
+    estadoSensores[i] = HIGH; //Se asume que high es libre
+    estadoAnteriorSensores[i] = HIGH;
   }
 
 }
 
 void loop() {
-  int lugaresLibres = 0; //valor inicial
-  int lugaresOcupados = 0; // valor inicial
+  
 
   // Ciclo for parta leer los valores de cada sensor 
   for (int i = 0; i < numSensores; i++){
     estadoSensores[i] = digitalRead(sensores[i]);
-    //Si estan ocupados, se incrementa uno en la variable lugares ocupados , si no , se aumenta uno en los libres
-    if (estadoSensores[i] == LOW){ //checar que si sea LOW cuando esta ocupado
-      lugaresOcupados++;
-    }else{
-      lugaresLibres++;
+    //checar si el resultado del sensor cambio
+    if (estadoSensores[i] != estadoAnteriorSensores[i]){
+
+      //si se libero un lugar
+      if (estadoAnteriorSensores[i] == LOW && estadoSensores[i] == HIGH){ 
+        lugaresOcupados--;
+        lugaresLibres++; 
+      }
+      //si se ocupo un lugar
+      else if(estadoAnteriorSensores[i] == HIGH && estadoSensores[i] == LOW){ 
+        lugaresOcupados++;
+        lugaresLibres--;
+      }
     }
-  }
+    }
+  
   Serial.print("Lugares Libres: ");
   Serial.println(lugaresLibres);
   Serial.print("Lugares Ocupados: ");
   Serial.println(lugaresOcupados);
+  
 
-  delay(10000)//verificar si el delay es correcto con pruebas 
+  delay(5000);//verificar si el delay es correcto con pruebas 
 }
